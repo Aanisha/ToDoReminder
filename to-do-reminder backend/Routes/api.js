10ff7f02cf443421d4c9702cc6306d7dec98c5ef
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2020-06-02 12:17:17 +0530

    Added new endpoints

--- to-do-reminder backend/Routes/api.js
+++ to-do-reminder backend/Routes/api.js
@@ -1,18 +1,102 @@
 const express = require ('express');
 const router = express.Router();
 const Todo = require('../models/todo');
+const Login = require('../models/login');
 
-router.get('/todos', (req, res, next) => {
+router.get('/login', (req, res, next) => {
 
   //this will return all the data, exposing only the id and action field to the client
-  Todo.find({}, 'action')
+  var userName=req.body.userName;
+  var password=req.body.password;
+
+  Login.findOne({userName:userName,password:password},function(err,user){
+
+    if(err)
+    {
+      return res.status(500).send();
+
+  }
+  if(!user)
+  {
+      res.json({
+      error: "Incorrect userName or password dosen't exist"
+    })
+
+
+  }
+  else
+  {
+      
+        /*Todo.find({userName:userName},function(err,user)
+        {
+
+               if(err)
+               {
+                     return res.status(500).send();
+
+               }
+               else
+               {
+                   res.send(user);
+               }
+        });*/
+
+        res.json({
+                isLoggedin: true,
+                id:user.id,
+                userName:user.userName
+          })
+  }
+         
+
+});
+
+});
+
+router.post('/signin', (req, res, next) => {
+  if(req.body){
+    Login.create(req.body)
+      .then(data => res.json(data))
+      .catch(next)
+  }else {
+    res.json({
+      error: "input fields is empty"
+    })
+  }
+});
+
+router.get('/loggedin', (req, res, next) => {
+
+  //this will return all the data, exposing only the id and action field to the client
+  Login.find({})
     .then(data => res.json(data))
     .catch(next)
 });
 
-router.post('/todos', (req, res, next) => {
-  if(req.body.action){
-    Todo.create(req.body)
+router.get('/todos/:user', (req, res, next) => {
+
+  //this will return all the data, exposing only the id and action field to the client
+  /*Todo.find({})
+    .then(data => res.json(data))
+    .catch(next)*/
+
+    Todo.find({"userName": req.params.user})
+    .then(data => res.json(data))
+    .catch(next)
+});
+
+router.post('/todos/:user', (req, res, next) => {
+  if(req.body.action && req.body.date && req.body.label)
+  {
+  var data={
+
+      userName:req.params.user,
+      action:req.body.action,
+      date:req.body.date,
+      label:req.body.label
+  }
+  
+    Todo.create(data)
       .then(data => res.json(data))
       .catch(next)
   }else {
@@ -21,11 +105,12 @@ router.post('/todos', (req, res, next) => {
     })
   }
 });
+  
 
 router.delete('/todos/:id', (req, res, next) => {
   Todo.findOneAndDelete({"_id": req.params.id})
     .then(data => res.json(data))
     .catch(next)
-})
+});
 
 module.exports = router;
