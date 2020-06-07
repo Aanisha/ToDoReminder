6cdab7eeb97a3003c00e9b3369a1172765ce235d
Aanisha Bhattacharyya <43934116+Aanisha@users.noreply.github.com>
2020-06-07 13:30:26 +0530

    Added fields for date and label

--- client/src/pages/components/InputTodo.js
+++ client/src/pages/components/InputTodo.js
@@ -10,13 +10,21 @@ class InputTodo extends React.Component {
     }
   }
 
-  updateText = (text) => {
+  /*updateText = (text) => {
     let data = this.state.data
     data.todo = text
     this.setState({
       data: data
     })
-  }
+  }*/
+
+  updateForm = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        let data = this.state.data;
+        data[nam] = val;
+        this.setState({ data: data });
+    }
 
   createTodo = () => {
     axios.post('/api/addtodo', this.state.data)
@@ -29,9 +37,40 @@ class InputTodo extends React.Component {
   render() {
     return (
       <div className="todo-list" style={{display: 'flex'}}>
-        <input className="input is-small" type="text" onChange={(e) => this.updateText(e.target.value)} value={this.state.data.todo} placeholder="Enter todo" />
-        &nbsp;
-        <input className="button is-block is-info is-small" onClick={this.createTodo} type="button" value="Add" />
+
+        <form onSubmit={this.createTodo}>    
+
+                                    <div className="field">
+                                        <div className="control">
+                                            <input className="input is-normal"  onChange={this.updateForm} name="todo" type="text" placeholder="Enter Todo" required autoFocus=""/>
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="field">
+                                        <div className="control">
+                                            <input className="input is-normal"  onChange={this.updateForm} name="due" type="date" placeholder="Date" required autoFocus=""/>
+                                        </div>
+                                         
+                                    </div>
+                                    <div className="field">
+                                        <div className="control">
+                                            
+                                        <label for="label">Choose a label:</label>
+
+                                                  <select name="label" id="label" onChange={this.updateForm}>
+                                                      <option value="Personal">Personal</option>
+                                                      <option value="Work">Work</option>
+                                                      <option value="Shopping">Shopping</option>
+                                                      <option value="Others">Others</option>
+                                                  </select> 
+                                        </div>
+                                    </div>
+
+                                    
+                                    <input className="button is-block is-info is-small" onClick={this.createTodo} type="button" value="Add" />
+                                </form>
+
+       
       </div>
     )
   }
